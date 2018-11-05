ff0c83b67b2695894a6cfae13a76e3261f8da09f
Bram van den Heuvel <bram.vdh1999@gmail.com>
2018-11-05 16:45:45 +0100

    Add template for importing files

---
+++ resources/get.py
@@ -0,0 +1,10 @@
+def batteries(neighbourhood):
+    """Gain a list of batteries from a given neighbourhood. The neighbourhood is an integer."""
+
+    pass # TODO
+
+
+def houses(neighbourhood):
+    """Gain a list of houses from a given neighbourhood. The neighbourhood is an integer."""
+
+    pass # TODO
--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+from resources import get
 import unittest
 
 class Test_Algorithms(unittest.TestCase):
@@ -15,3 +16,26 @@ class Test_Algorithms(unittest.TestCase):
 
     def good_thing_this_function_is_ignored(self):
         self.assertEqual(1,2)
+    
+    
+    # Change this name when you're working on this part.
+    def t3st_importer(self):
+        self.assertEqual(get.batteries(1),[[38,12], [43,13], [42,3], [49,23], [3,45]])
+        self.assertEqual(get.batteries(2),[[19,20], [1,36], [34,49], [41,21], [26,22]])
+        self.assertEqual(get.batteries(3),[[18,34], [32,11], [41,1], [3,35], [39,41]])
+
+        first_house = get.houses(1)[0]
+        second_house = get.houses(1)[1]
+        random_house = get.houses(3)[2]
+
+        self.assertEqual(first_house.x, 34)
+        self.assertEqual(first_house.y, 47)
+        self.assertEqual(first_house.output, 53.97543253)
+
+        self.assertEqual(second_house.x, 24)
+        self.assertEqual(second_house.y, 22)
+        self.assertEqual(second_house.output, 66.13020717)
+
+        self.assertEqual(random_house.x, 25)
+        self.assertEqual(random_house.y, 31)
+        self.assertEqual(random_house.output, 51.81241388)
