794550946038ed5c95b36bad722d7b0a9bc3d439
Bram van den Heuvel <bram.vdh1999@gmail.com>
2018-11-06 23:15:43 +0100

    Add House class to import file
    This will allow the user to manipulate the "house" data as an easy object.

--- resources/get.py
+++ resources/get.py
@@ -1,3 +1,9 @@
+class House:
+    def __init__(self,x,y,output):
+        self.x      = int(x)
+        self.y      = int(y)
+        self.output = float(output)
+
 def batteries(neighbourhood):
     """Gain a list of batteries from a given neighbourhood. The neighbourhood is an integer."""
 
