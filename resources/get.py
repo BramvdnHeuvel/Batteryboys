db90aac7bcecb35117df428645790ab9452c88fc
Bram van den Heuvel <bram.vdh1999@gmail.com>
2018-11-08 19:17:36 +0100

    Expand House class

--- resources/get.py
+++ resources/get.py
@@ -1,8 +1,16 @@
 class House:
     def __init__(self,x,y,output):
-        self.x      = int(x)
-        self.y      = int(y)
-        self.output = float(output)
+        self.x              = int(x)
+        self.y              = int(y)
+        self.output         = float(output)
+        self.connection     = None
+        self.route          = []
+    
+    def connect(self,connected_obect):
+        """Connect the house to another object. The route also needs to be determined here."""
+
+        pass # TODO
+
 
 def batteries(neighbourhood):
     """Gain a list of batteries from a given neighbourhood. The neighbourhood is an integer."""
