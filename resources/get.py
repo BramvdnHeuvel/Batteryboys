cabb364c0272fb22e34641ac1f7d69e58c83d39e
Bram van den Heuvel <bram.vdh1999@gmail.com>
2018-11-08 19:17:57 +0100

    Add Battery class to repository

--- resources/get.py
+++ resources/get.py
@@ -12,6 +12,19 @@ class House:
         pass # TODO
 
 
+class Battery:
+    def __init__(self,x,y,max_capacity):
+        self.x              = int(x)
+        self.y              = int(y)
+        self.max_capacity   = int(max_capacity)
+        self.power          = int(max_capacity)
+
+    def store(self,power_amount):
+        """Remove a certain amount of capacity that is being taken."""
+        self.power = self.power - power_amount
+
+# ---------------------------------------------
+
 def batteries(neighbourhood):
     """Gain a list of batteries from a given neighbourhood. The neighbourhood is an integer."""
 
