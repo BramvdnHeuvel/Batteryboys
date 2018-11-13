a587eed7fbc32e1aedf1ff6efa4a71424e41f57e
Bram van den Heuvel <bram.vdh1999@gmail.com>
2018-11-13 10:03:47 +0100

    Add __find_object function to Map class

--- classes.py
+++ classes.py
@@ -8,3 +8,16 @@ class Map:
     def render(self):
         """Visualize the board, including any potentially made connections"""
         pass # TODO
+    
+    def __find_object(self,x,y):
+        """Find the object that is located on a given location."""
+        for battery in self.batteries:
+            if x == battery.x and y == battery.y:
+                return battery
+        
+        for house in self.houses:
+            if x == house.x and y == house.y:
+                return house
+        
+        return None
+
