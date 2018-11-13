bf2f96c76832b82d115494e0c637a87a468d92cb
Bram van den Heuvel <bram.vdh1999@gmail.com>
2018-11-13 10:04:41 +0100

    Add __connect function to Map class

--- classes.py
+++ classes.py
@@ -20,4 +20,20 @@ class Map:
                 return house
         
         return None
-
+
+    def __connect(self,x1,y1,x2,y2,must_connect_to_battery=True):
+        """Connect the object of one location to another."""
+        house = self.__find_object(x1,y1)
+        battery = self.__find_object(x2,y2) # Note: doesn't have to be battery, per say
+
+        if house is None:
+            raise TypeError("Could not find house that needed to be connected.")
+        if battery is None:
+            raise TypeError("Could not find house/battery that needed to be connected to.")
+        
+        if must_connect_to_battery and battery.__class__.__name__ != "Battery":
+            raise TypeError("Cannot connect to a non-Battery object")
+
+        house.connect(battery)
+        self.moneyspent += manhattan_distance(x1,y1,x1,y1) * config.cost_per_grid_section
+    
