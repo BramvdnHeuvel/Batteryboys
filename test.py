2d2a6d402e151a6a04cbf532cf576d6e6dfc1314
Bram van den Heuvel <bram.vdh1999@gmail.com>
2018-11-16 14:40:53 +0100

    Add unittests for 'House' and 'Battery' classes

--- test.py
+++ test.py
@@ -2,6 +2,9 @@ from resources import get
 import scheme
 import unittest
 
+from resources.BatteryClass import Battery
+from HouseClass import House
+
 class Test_Algorithms(unittest.TestCase):
 
     # This class tests functions and makes sure they work.
@@ -18,12 +21,28 @@ class Test_Algorithms(unittest.TestCase):
     def good_thing_this_function_is_ignored(self):
         self.assertEqual(1,2)
     
-    
+    def test_battery_class(self):
+        battery = Battery(1,2,1500)
+
+        self.assertEqual(battery.x, 1)
+        self.assertEqual(battery.y, 2)
+        self.assertEqual(battery.max_capacity, 1500)
+        self.assertEqual(battery.power, 1500)
+
+        battery.store(500)
+
+        self.assertEqual(battery.max_capacity, 1500)
+        self.assertEqual(battery.power, 1000)
+
+    def test_house_class(self):
+        house = Houses()
+
+
     # Change this name when you're working on this part.
     def t3st_importer_batteries(self):
-        self.assertEqual(get.batteries(1),[[38,12], [43,13], [42,3], [49,23], [3,45]])
-        self.assertEqual(get.batteries(2),[[19,20], [1,36], [34,49], [41,21], [26,22]])
-        self.assertEqual(get.batteries(3),[[18,34], [32,11], [41,1], [3,35], [39,41]])
+        self.assertEqual(get.batteries(1),[[38,12,1507.0], [43,13,1507.0], [42,3,1507.0], [49,23,1507.0], [3,45,1507.0]])
+        self.assertEqual(get.batteries(2),[[19,20,1508.25], [1,36,1508.25], [34,49],1508.25, [41,21,1508.25], [26,22,1508.25]])
+        self.assertEqual(get.batteries(3),[[18,34,1506.75], [32,11,1506.75], [41,1,1506.75], [3,35,1506.75], [39,41,1506.75]])
 
     # Change this name when you're working on this part.
     def t3st_importer_houses(self):
