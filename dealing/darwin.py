3378ea78bf7eb096236c5c9e9ea05b1a5b4cff1a
Bram van den Heuvel <bram.vdh1999@gmail.com>
2018-11-17 00:02:21 +0100

    Add hill climbing-alike algorithm to find best fit

--- dealing/darwin.py
+++ dealing/darwin.py
@@ -1,22 +1,82 @@
 from sample import FakeBattery, FakeHouse
 import random
 
-battery_list = [FakeBattery(0,3),  FakeBattery(1,4)]
-house_list = [FakeHouse(2), FakeHouse(2), FakeHouse(3)]
+battery_list = [FakeBattery(7),  FakeBattery(7)]
+house_list = [FakeHouse(2), FakeHouse(5), FakeHouse(1), FakeHouse(2), FakeHouse(4)]
 
-def natural_selection(parent_one,parent_two,off_put=0.2):
-    next_generation = []
+def __natural_selection(parent_one, parent_two, random_size=len(battery_list), off_put=0.2):
+    child = []
 
     for house_parents in zip(parent_one,parent_two):
         decision = random.random()
 
+        # Choose a random genome
         if decision < (1-off_put)/float(2):
-            next_generation.append(house_parents[0])
+            child.append(house_parents[0])
         elif decision < (1-off_put):
-            next_generation.append(house_parents[1])
+            child.append(house_parents[1])
         else:
-            next_generation.append(0)
+            child.append(random.randint(0,random_size-1))
     
-    return next_generation
+    return child
 
-print(natural_selection(range(10),range(11,20)))
+
+def __calculate_score(individual, house_list=house_list, battery_list=battery_list):
+    houses = house_list
+    batteries = battery_list
+
+    for choice in zip(houses,individual):
+        house = choice[0]
+        battery = batteries[choice[1]]
+
+        # Connect the house if it still fits.
+        if house.output <= battery.power:
+            house.connect(battery)
+    score = sum([len(battery) for battery in batteries])
+
+    for battery in batteries:
+        battery.reset()
+
+    return score
+
+
+def __generate_first_generation(houses=house_list, batteries=battery_list, population=2):
+    return [[random.randint(0,len(batteries)-1) for j in houses] for i in range(2*population)]
+
+def __generate_new_generation(old_generation):
+    new_generation = []
+    
+    for i in range(len(old_generation) // 4):
+        parent_one = old_generation[2*i][1]
+        parent_two = old_generation[2*i+1][1]
+
+        child1 = __natural_selection(parent_one, parent_two)
+        child2 = __natural_selection(parent_one, parent_two)
+        child3 = __natural_selection(parent_one, parent_two)
+        child4 = __natural_selection(parent_one, parent_two)
+
+        new_generation.append(child1)
+        new_generation.append(child2)
+        new_generation.append(child3)
+        new_generation.append(child4)
+
+    return new_generation    
+
+def generate_algorithm(amount=20, maximum=len(house_list)):
+    generation = __generate_first_generation()
+    generation = [(__calculate_score(individual),individual) for individual in generation]
+    generation.sort(reverse=True)
+
+    for i in range(amount):
+        if generation[0][0] == maximum:
+            break
+
+        print("Generation {}:\t{}".format(i,generation))
+
+        generation = __generate_new_generation(generation)
+        generation = [(__calculate_score(individual),individual) for individual in generation]
+        generation.sort(reverse=True)
+
+    return generation[0]
+
+print(generate_algorithm())
--- dealing/sample.py
+++ dealing/sample.py
@@ -1,15 +1,20 @@
 class FakeBattery:
-    def __init__(self,id,max_capacity):
-        self.id = id
+    def __init__(self,max_capacity):
         self.max_capacity = max_capacity
         self.power = max_capacity
+        self.connections = []
+    
+    def __len__(self):
+        return len(self.connections)
+    
+    def reset(self):
+        self.power = self.max_capacity
         self.connections = []
 
 class FakeHouse:
     def __init__(self,output):
         self.output = output
-        self.connection = 0
     
     def connect(self,battery):
         battery.connections.append(self)
-        self.connection = battery.id
+        battery.power = battery.power - self.output
