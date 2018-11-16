4166c3b67ede6105547d25891d6063e2b6c4337c
MaxB4 <max.baneke@hotmail.com>
2018-11-16 13:29:31 +0100

    deleted battery and house class

--- get.py
+++ get.py
@@ -1,8 +1,9 @@
 from BatteryClass import Battery
+from HouseClass import House
 import re
 INPUT='wijk1_batterijen.txt'
 
-class House:
+class get:
     def __init__(self,x,y,output):
         self.x              = int(x)
         self.y              = int(y)
@@ -21,22 +22,13 @@ class House:
 
         pass # TODO
 
-
-class Battery:
-    def __init__(self,x,y,max_capacity):
-        self.x              = int(x)
-        self.y              = int(y)
-        self.max_capacity   = int(max_capacity)
-        self.power          = int(max_capacity)
-
-
+    
     def store(self,power_amount):
         """Remove a certain amount of capacity that is being taken."""
         self.power = self.power - power_amount
 
-# ---------------------------------------------
 
-def batteries(neighbourhood):
+    def load_batteries(self, filename):
     """Gain a list of batteries from a given neighbourhood. The neighbourhood is an integer."""
     
     battery_list=[]
