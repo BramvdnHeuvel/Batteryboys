57d623ecb515f6464687b623bc9339c500a20e34
misterwhybe <wiebejelsma@hotmail.com>
2018-11-16 17:24:30 +0100

    Revert "iets betere versie object house, nu returnt ie het per rij"
    This reverts commit a12494b6d9f9ad8a197a0e693e93604d0c9b2a69.

--- get.py
+++ get.py
@@ -6,48 +6,49 @@ INPUT='wijk1_batterijen.txt'
 INPUT_HOUSE = 'resources/wijk1_huizen.csv'
 
 
-# class get:
-#     def __init__(self,x,y,output):
-#         self.x              = int(x)
-#         self.y              = int(y)
-#         self.output         = float(output)
-#         self.connection     = None
-#         self.route          = []
+class get:
+    def __init__(self,x,y,output):
+        self.x              = int(x)
+        self.y              = int(y)
+        self.output         = float(output)
+        self.connection     = None
+        self.route          = []
     
-def connect(self,connected_obect):
-    """Connect the house to another object. The route also needs to be determined here."""
+    def connect(self,connected_obect):
+        """Connect the house to another object. The route also needs to be determined here."""
 
-    pass # TODO
-
-
-def store(self,power_amount):
-    """Remove a certain amount of capacity that is being taken."""
-    self.power = self.power - power_amount
-
-
-def load_batteries(self, filename):
-    """Gain a list of batteries from a given neighbourhood. The neighbourhood is an integer."""
+        pass # TODO
     
-    battery_list=[]
-    with open(INPUT,'r') as file:
-        next(file)
-        for line in file:
-            result = re.findall(r'\d+.?\d*\b', line)
-            x = result[0]
-            y = result[1]
-            capacity = result[2]
-            battery_list.append(Battery(x, y, capacity))
-        return(battery_list)
+    
+    def store(self,power_amount):
+        """Remove a certain amount of capacity that is being taken."""
+        self.power = self.power - power_amount
 
 
-def load_houses(neighbourhood):
-    """Gain a list of houses from a given neighbourhood. The neighbourhood is an integer."""
-    data = pd.read_csv(INPUT_HOUSE)
-    huizen = []
-    for index, row in data.iterrows():
-        x = row['x']
-        y = row['y']
-        output = row['max. output']
-        z = [x, y, output]
-        huizen.append(House(x, y, output))
-    return huizen    
+    def load_batteries(self, filename):
+        """Gain a list of batteries from a given neighbourhood. The neighbourhood is an integer."""
+        
+        battery_list=[]
+        with open(INPUT,'r') as file:
+            next(file)
+            for line in file:
+                result = re.findall(r'\d+.?\d*\b', line)
+                x = result[0]
+                y = result[1]
+                capacity = result[2]
+                battery_list.append(Battery(x, y, capacity))
+            return(battery_list)
+
+
+    def load_houses(neighbourhood):
+        """Gain a list of houses from a given neighbourhood. The neighbourhood is an integer."""
+        data = pd.read_csv(INPUT_HOUSE)
+        df = pd.DataFrame(data)
+        huizen = []
+        for row in df:
+            x = df['x']
+            y = df['y']
+            output = df['max. output']
+            huizen.append(House(x, y, output))
+        print(huizen)
+        pass # TODO
