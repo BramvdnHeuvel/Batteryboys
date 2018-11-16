608d6aa779317c85d430eb4dec159df149708f06
MaxB4 <max.baneke@hotmail.com>
2018-11-16 13:10:00 +0100

    deleted double class

--- classes.py
+++
@@ -1,9 +0,0 @@
-   
-class House(object):
-    def __init__(self, X, Y, output, connection):
-        self.X = X
-        self.Y = Y
-        self.output = output
-        self.connection = connection 
-
-        pass    
