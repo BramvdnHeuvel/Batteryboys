d961548436b37d94857b4627eb17c2dad529bf5d
MaxB4 <max.baneke@hotmail.com>
2018-11-19 15:12:01 +0100

    added a test123 file to test grid

--- test123.py
+++ test123.py
@@ -2,11 +2,12 @@ from classes.battery import Battery
 from classes.house import House
 import re
 import pandas as pd
+import matplotlib.pyplot as plt
 
-INPUT='/resources/wijk1_batterijen.txt'
-INPUT_HOUSE = '/resources/wijk1_huizen.csv'
+INPUT='resources/wijk1_batterijen.txt'
+INPUT_HOUSE = 'resources/wijk1_huizen.csv'
 
-
+# get battery location
 battery_list=[]
 with open(INPUT,'r') as file:
     next(file)
@@ -15,14 +16,27 @@ with open(INPUT,'r') as file:
         x = result[0]
         y = result[1]
         capacity = result[2]
-        battery_list.append(Battery(x, y, capacity))
-print(battery_list)
+        z= [x, y, capacity]
+        battery_list.append(z)
+    print(battery_list)
 
+# get house location
 data = pd.read_csv(INPUT_HOUSE)
 huizen = []
 for index, row in data.iterrows():
     x = row['x']
     y = row['y']
     output = row['max. output']
-    huizen.append(House(x, y, output))
+    z= [x, y, capacity]
+    huizen.append(z)
 print(huizen)
+
+# plot batteries
+x = list(map(lambda x: x[0], battery_list))
+y = list(map(lambda x: x[1], battery_list))
+
+plt.scatter(x, y)
+
+plt.grid(True)
+
+plt.show()
