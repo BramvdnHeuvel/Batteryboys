de77972ac252d318ed770c94fdea2352800097c1
misterwhybe <wiebejelsma@hotmail.com>
2018-11-20 11:12:35 +0100

    house class code removed

--- classes/house.py
+++ classes/house.py
@@ -6,37 +6,4 @@ class House:
         self.x = x 
         self.y = y 
         self.output = output
-        #self.connection = connection # (bool)
         
-    # def X(self):
-
-
-    #     file = pd.read_csv("wijk1_huizen.csv")
-    #     frame = pd.DataFrame(file)
-    #     # print(frame)
-    #     X_axis = frame["x"]
-    #     print(X_axis)
-    #     for x in X_axis:
-    #         X_coordinate = x
-    #         print(X_coordinate)
-    #     return X_coordinate
-
-
-    # def Y(self):
-    #     file = pd.read_csv("wijk1_huizen.csv")
-    #     frame = pd.DataFrame(file)
-    #     Y_axis = frame["y"]
-    #     for y in Y_axis:
-    #         Y_coordinate = y
-    #     return Y_coordinate
-
-
-    # def Output(self):
-    #     file = pd.read_csv("wijk1_huizen.csv")
-    #     frame = pd.DataFrame(file)
-    #     max_output = frame["max. output"]
-
-    #     for z in max_output:
-    #         output = z
-    #     return output
-         
