8e9d86c2be5a16faf3be68c352ebd8501631bb88
misterwhybe <wiebejelsma@hotmail.com>
2018-11-21 23:28:01 +0100

    def foo in main een algoritme ingezet

--- main.py
+++ main.py
@@ -6,9 +6,17 @@ grid = Map(1)
 @grid.execute
 def foo(self,houses,batteries):
     
+    connected = []
+    for battery in self.batteries:
+        for house in self.houses:
+            if not house in connected and battery.capacity > house.output:
+                battery.capacity -= house.output
+                print(battery.capacity)
+                connected.append(house)
+                print(house)
+    print(len(connected)) 
+
     # Write a specific algorithm to execute here.
-    print(houses[0])
-    print("En nu de batteries!")
-    print(batteries[4])
+    # nu print het 150 huizen, allemaal in een v/d 5 batterijen. nog niet welke waar.
 
 grid.start()
