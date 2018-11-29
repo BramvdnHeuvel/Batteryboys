b442ecefd363353cb2244d5e872db7ec3a0101e0
MaxB4 <max.baneke@hotmail.com>
2018-11-29 15:49:36 +0100

    small adjustments

---
+++ Visualizer.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+from classes.battery import Battery
+from classes.house import House
+
+
+def visualization(self, houses, batteries):
+    for house in houses:
+        print(house)
