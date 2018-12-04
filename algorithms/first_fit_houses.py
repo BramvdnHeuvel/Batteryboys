24c2c6f8035f9a5d8b5e212fb1dea8bca6066e35
Bram <bram.vdh1999@gmail.com>
2018-12-04 10:27:08 +0100

    Allow first fit algorithms to display money spent

--- algorithms/first_fit_houses.py
+++ algorithms/first_fit_houses.py
@@ -7,8 +7,8 @@ def first_fit_houses(self, houses, batteries):
 
     for house in houses:
         for battery in batteries:
-            if not house in connected and battery.capacity > house.output:
-                house.connection(battery)
-                battery.capacity -= house.output
+            if not house in connected and battery.power > house.output:
+                self.connect(house, battery)
                 connected.append(house)
     print(len(connected))
+    print(self.moneyspent)
