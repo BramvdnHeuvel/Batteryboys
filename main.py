021cb6564cded614c3f9f9da225720ec812547da
MaxB4 <max.baneke@hotmail.com>
2018-12-07 17:14:28 +0100

    lower bound

--- main.py
+++ main.py
@@ -6,43 +6,29 @@ from algorithms.first_fit_houses import first_fit_houses
 from algorithms.first_fit_batteries import first_fit_batteries
 from algorithms.genetic_race import find_raced_fit
 from algorithms.hillclimber import hillclimber
+from algorithms.bounds import first_distance
 import random
 
 from algorithms.differential_evolution import DEGeneration
 
 first_generation = []
 
-# while len(first_generation) < 4:
-#     try:
-#         grid = Map(1)
-#         grid.execute(first_fit_batteries)
-#         grid.execute(hillclimber)
-#         grid.start()
-#         print(f"Before: {grid.moneyspent}\tAfter: {grid.refresh_cost()}")
-
-#         if [house.id for house in grid.houses if house.connected == None] == []:
-#             print(f"Found a child that costs {grid.moneyspent}!")
-#             first_generation.append(grid.get_list())
-#     except AttributeError:
-#         print("Failed attempt.")
-
 grid = Map(1)
-grid.execute(first_fit_batteries)
-grid.execute(hillclimber)
+grid.execute(first_distance)
 grid.start()
+grid.visualize()
 
-generation = DEGeneration(grid.houses, grid.batteries, first_generation, mutation=0.03)
-print(generation.find_best_one())
-scores = []
+# if False:
+#     while len(first_generation) < 4:
+#         try:
+#             grid = Map(1)
+#             grid.execute(first_fit_batteries)
+#             grid.execute(hillclimber)
+#             grid.start()
+#             print(f"Before: {grid.moneyspent}\tAfter: {grid.refresh_cost()}")
 
-for i in range(100000):
-    generation = next(generation)
-    if (i + 1) % 100 == 0:
-        print(f"Reached generation {i+1}!")
-    scores.append(-1*generation.find_best_one()[1][2])
-
-import matplotlib.pyplot as plt
-
-print(generation.find_best_one())
-plt.plot(scores)
-plt.show()
+#             if [house.id for house in grid.houses if house.connected == None] == []:
+#                 print(f"Found a child that costs {grid.moneyspent}!")
+#                 first_generation.append(grid.get_list())
+#         except AttributeError:
+#             print("Failed attempt.")
