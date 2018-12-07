c9a318e70b7188a181f458fcecc45d86aac86138
MaxB4 <max.baneke@hotmail.com>
2018-12-07 17:41:59 +0100

    best version

--- algorithms/bounds.py
+++ algorithms/bounds.py
@@ -1,33 +1,24 @@
+import matplotlib.pyplot as plt
 
-def first_distance(self,houses,batteries):
-    moneys = []
-    connected = []
-
-    
-    
+def first_distance(self,houses,batteries):  
+    total_distance = 0    
     for house in houses:
-        lowest_distance = 9000
-        lowest_battery = 8
+        lowest_distance = 1000000
         if distance(house, batteries[0]) < lowest_distance:
             lowest_distance = distance(house, batteries[0])
-            lowest_battery = 0
         if distance(house, batteries[1]) < lowest_distance:
             lowest_distance = distance(house, batteries[1])
-            lowest_battery = 1
         if distance(house, batteries[2]) < lowest_distance:
             lowest_distance = distance(house, batteries[2])
-            lowest_battery = 2
         if distance(house, batteries[3]) < lowest_distance:
             lowest_distance = distance(house, batteries[3])
-            lowest_battery = 3
         if distance(house, batteries[4]) < lowest_distance:
             lowest_distance = distance(house, batteries[4])
-            lowest_battery = 4
-        print(lowest_distance)
-        self.connect(house, batteries[lowest_battery])
-        
-       
-    
+        total_distance += lowest_distance
+    print(total_distance * 9)
+
+
+
 def distance(house, battery):
     return abs(house.x - battery.x) + abs(house.y - battery.y)
     
