dfbc78a17530f0e519113c81e0870dd464b5a467
Bram <bram.vdh1999@gmail.com>
2018-12-07 12:13:40 +0100

    Add battery switch algorithm

--- algorithms/batteryswitch.py
+++ algorithms/batteryswitch.py
@@ -3,3 +3,19 @@ from classes.house import House
 def batteryswitch(self, houses, batteries):
 # change x and y for batteries so that grid distance is minimal
 # if battery is full connect house to second closest battery
+    for battery in self.batteries:
+
+        x_values = [house.x for house in self.houses if house.connected.id == battery.id]
+        y_values = [house.y for house in self.houses if house.connected.id == battery.id]
+
+        x_values.sort()
+        y_values.sort()
+
+        x_index = (len(x_values) - 1)//2
+        y_index = (len(y_values) - 1)//2
+
+        median_x = x_values[x_index]
+        median_y = y_values[y_values]
+
+        battery.x = median_x
+        battery.y = median_y
