043cb36851fdab6a9c7e4646771f299245fc3219
Bram <bram.vdh1999@gmail.com>
2018-12-16 21:41:48 +0100

    Add ids to Batteries in unittests

--- test.py
+++ test.py
@@ -22,8 +22,9 @@ class Test_Algorithms(unittest.TestCase):
         self.assertEqual(1,2)
 
     def test_battery_class(self):
-        battery = Battery(1,2,1500)
+        battery = Battery(0,1,2,1500)
 
+        self.assertEqual(battery.id, 0)
         self.assertEqual(battery.x, 1)
         self.assertEqual(battery.y, 2)
         self.assertEqual(battery.capacity, 1500)
@@ -36,9 +37,9 @@ class Test_Algorithms(unittest.TestCase):
 
     # Change this name when you're working on this part.
     def test_importer_batteries(self):
-        self.assertEqual(get.batteries(1),[Battery(38,12,1507.0), Battery(43,13,1507.0), Battery(42,3,1507.0), Battery(49,23,1507.0), Battery(3,45,1507.0)])
-        self.assertEqual(get.batteries(2),[Battery(19,20,1508.25), Battery(1,36,1508.25), Battery(34,49,1508.25), Battery(41,21,1508.25), Battery(26,22,1508.25)])
-        self.assertEqual(get.batteries(3),[Battery(18,34,1506.75), Battery(32,11,1506.75), Battery(41,1,1506.75), Battery(3,35,1506.75), Battery(39,41,1506.75)])
+        self.assertEqual(get.batteries(1),[Battery(0,38,12,1507.0), Battery(1,43,13,1507.0), Battery(2,42,3,1507.0), Battery(3,49,23,1507.0), Battery(4,3,45,1507.0)])
+        self.assertEqual(get.batteries(2),[Battery(0,19,20,1508.25), Battery(1,1,36,1508.25), Battery(2,34,49,1508.25), Battery(3,41,21,1508.25), Battery(4,26,22,1508.25)])
+        self.assertEqual(get.batteries(3),[Battery(0,18,34,1506.75), Battery(1,32,11,1506.75), Battery(2,41,1,1506.75), Battery(3,3,35,1506.75), Battery(4,39,41,1506.75)])
 
     # Change this name when you're working on this part.
     def test_importer_houses(self):
@@ -58,16 +59,4 @@ class Test_Algorithms(unittest.TestCase):
         self.assertEqual(random_house.x, 25)
         self.assertEqual(random_house.y, 31)
         self.assertEqual(random_house.output, 51.81241388)
-
-    def test_dr_manhattan(self):
-        self.assertEqual(distance(10,0,0,0),10)
-        self.assertEqual(distance(0,10,0,0),10)
-        self.assertEqual(distance(0,0,10,0),10)
-        self.assertEqual(distance(0,0,0,10),10)
-        
-        self.assertEqual(distance(10,0,0,10),20)
-        self.assertEqual(distance(-10,0,0,0),10)
-        self.assertEqual(distance(-10,0,10,0),20)
-        self.assertEqual(distance(1,2,3,4),4)
-        self.assertEqual(distance(10,0,100,1000),1090)
 
